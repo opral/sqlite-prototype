75c8770119f5bd7d05b49876c84425a96944e3e2
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-07-04 11:27:28 -0400

    dont capitalize bundle ids

--- inlang-sdk/src/index.ts
+++ inlang-sdk/src/index.ts
@@ -2,10 +2,10 @@ export * from "./loadProject";
 export * from "./importProject";
 export { newProject } from "./newProject";
 import { humanId } from "human-id";
-export { jsonArrayFrom, jsonObjectFrom } from "kysely/helpers/sqlite";
 
 export function generateBundleId() {
   return humanId({
     separator: "_",
+    capitalize: false,
   });
 }
